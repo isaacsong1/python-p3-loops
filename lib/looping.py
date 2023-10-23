3b0e85a2fa8e2e42fce9308fc5a0fc113719dcb2
Isaac Song <isaacsongs10@gmail.com>
2023-10-23 14:54:50 -0700

    Loop lab complete

--- lib/looping.py
+++ lib/looping.py
@@ -2,12 +2,26 @@
 
 def happy_new_year():
     # code goes here!
-    pass
+    i = 10
+    while i > 0:
+        print(i)
+        i -= 1
+    print("Happy New Year!")
 
 def square_integers(int_list):
     # code goes here!
-    pass
+    return [num ** 2 for num in int_list]
 
 def fizzbuzz():
     # code goes here!
-    pass
+    for i in range(1, 101):
+        if not i % 3 and not i % 5:
+            print("FizzBuzz")
+        elif not i % 3:
+            print("Fizz")
+        elif not i % 5:
+            print("Buzz")
+        else:
+            print(i)
+
+fizzbuzz()
